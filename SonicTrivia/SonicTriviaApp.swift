345769f3ca2e2ec7f130b12327e769680e5b2e70
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-05-03 08:32:56 -0500

    Initial Commit

---
+++ SonicTrivia/SonicTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicTriviaApp.swift
+//  SonicTrivia
+//
+//  Created by Student on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SonicTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
