c48eba974dac12b1f974f2e9de86521449a9e895
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-05-09 10:04:53 -0500

    commit 2

--- SonicTrivia/ContentView.swift
+++ SonicTrivia/ContentView.swift
@@ -21,10 +21,7 @@ struct ContentView: View {
     var body: some View {
         
         ZStack{
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
-            ], center: .top, startRadius: 200, endRadius: 700)
+            LinearGradient(gradient: Gradient(colors: [.red, .blue, .green]), startPoint: .top, endPoint: .bottom)
             .ignoresSafeArea()
             
             VStack{
